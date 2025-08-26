a8d46fcf0473356141fd6c6abd13d2bb5a883914
Adam Anwar <mr.adam.anwar@gmail.com>
2025-08-26 20:43:26 +0800

    feat(backend): add initial FastAPI app entrypoint
    Setup FastAPI instance in main.py
    
    Added root endpoint (/) returning Hello World
    
    Imported SQLAlchemy and Pydantic for future database models and validation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from typing import List, Optional
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base, Session
 
 app = FastAPI()
 
